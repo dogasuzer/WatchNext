47ef0ab7901e942c69188530482a4d7a56c0d71f
dogasuzer <doga.suzer@hotmail.com>
2023-05-13 22:32:43 +0300

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,21 @@
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Roboto_Mono } from 'next/font/google';
+import Head from 'next/head';
+
+const robotoMono = Roboto_Mono({
+  weight: '400',
+  subsets: ['latin']
+});
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <div className={robotoMono.className}>
+      <Head>
+        <title>watchNext</title>
+        <link rel="icon/" href="/public/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
+}
---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <main>
+        <h1 className="text-2xl text-green-500">watchNext</h1>
+      </main>
+    </div>
+  );
+};
+export default Home;
