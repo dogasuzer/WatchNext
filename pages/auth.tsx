35bd13bf9e2410b177d6df532d21261709e92c0a
dogasuzer <doga.suzer@hotmail.com>
2023-05-17 22:35:32 +0300

    styling changes

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -5,6 +5,7 @@ import { getSession, signIn } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import { FcGoogle } from 'react-icons/fc';
 import Input from '@/components/input';
+import Image from 'next/image';
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -47,7 +48,7 @@ const Auth = () => {
         callbackUrl: '/'
       });
 
-      router.push('/profiles');
+      router.push('');
     } catch (error) {
       console.log(error);
     }
@@ -68,13 +69,17 @@ const Auth = () => {
   }, [email, name, password, login]);
 
   return (
-    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
-      <div className="bg-black w-full h-full lg:bg-opacity-50">
-        <nav className="px-12 py-5">
-          <img src="/images/logo.png" className="h-12" alt="Logo" />
-        </nav>
-        <div className="flex justify-center">
-          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+    <div className="  h-full w-full lg:bg-[url('/images/hero.jpg')] lg:bg-no-repeat bg-center lg:bg-fixed lg:bg-cover ">
+      <div className=" bg-black w-full h-full lg:pb-20 lg:bg-opacity-50 ">
+        <Image
+          src="/images/logo.png"
+          className="hidden lg:flex "
+          width={400}
+          height={200}
+          alt="Logo"
+        />
+        <div className="flex  justify-center items-center">
+          <div className=" bg-black bg-opacity-70  content-center  bg-opacity-70 px-16 py-16 my-48 lg:my-6 lg:w-2/5 lg:max-w-md rounded-md w-full ">
             <h2 className="text-white text-4xl mb-8 font-semibold">
               {variant === 'login' ? 'Sign in' : 'Register'}
             </h2>
@@ -111,15 +116,16 @@ const Auth = () => {
             </button>
             <div className="flex flex-row items-center gap-4 mt-8 justify-center">
               <div
-                onClick={() => signIn('google', { callbackUrl: '/profiles' })}
-                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+                onClick={() => signIn('google', { callbackUrl: '' })}
+                className="w-full py-3 bg-white  rounded-md flex items-center justify-center cursor-pointer hover:opacity-80 transition"
               >
-                <FcGoogle size={32} />
+                <div className="px-6 lg:px-2 ">Sing in with Google</div>
+                <FcGoogle size={24} />
               </div>
             </div>
             <p className="text-neutral-500 mt-12">
               {variant === 'login'
-                ? 'First time using Netflix?'
+                ? 'First time using watchNext?'
                 : 'Already have an account?'}
               <span
                 onClick={toggleVariant}
