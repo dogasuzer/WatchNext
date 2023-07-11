37141318592dfd620cdb86844e12cba5fff04f85
dogasuzer <doga.suzer@hotmail.com>
2023-07-11 18:07:55 +0300

    bug fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Roboto_Mono } from 'next/font/google';
 import Head from 'next/head';
-import { SessionProvider } from 'next-auth/react';
 
 const robotoMono = Roboto_Mono({
   weight: '400',
@@ -12,12 +11,10 @@ const robotoMono = Roboto_Mono({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={robotoMono.className}>
-      <SessionProvider session={pageProps.session}>
-        <Head>
-          <link rel="icon/" href="/public/favicon.ico" />
-        </Head>
-        <Component {...pageProps} />
-      </SessionProvider>
+      <Head>
+        <link rel="icon/" href="/public/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
     </div>
   );
 }
